b594f6771e2414fee8f7a95c103ee7ba24b53f4e
Eduardoespl <eduardoesp3317@gmail.com>
2024-02-18 15:02:21 -0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const feliz = document.getElementById('feliz');
+const triste = document.getElementById('triste');
+const enojada = document.getElementById('enojada');
+const cansada = document.getElementById('cansada');
+const aburrida = document.getElementById('aburrida');
+const estresada = document.getElementById('estresada');
+
+function felizClick(){
+    alert("Estoy feliz")
+}
+
+function tristeClick(){
+    alert("Estoy triste")
+}
+
+function enojadaClick(){
+    alert("Estoy enojada")
+}
+
+function cansadaClick(){
+    alert("Estoy cansada")
+}
+
+function aburridaClick(){
+    alert("Estoy aburrida")
+}
+
+function estresadaClick(){
+    alert("Estoy estresada")
+}
